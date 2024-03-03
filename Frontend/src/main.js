2b7df06bf9d7d96fb055ff87100eceeb0a1dc1fc
aronmero <aronmedinasp@gmail.com>
2024-03-03 16:10:51 +0000

    Frontend style folder

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import router from "@/router/index.js";
 import App from "./App.vue";
-import "./style.scss";
+import "@/style.scss";
 
 const pinia = createPinia();
 const app = createApp(App);
