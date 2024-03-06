62c41c010e6558f5ecf67cc58160d6142e8e6844
Christian <chrisrengifo188@gmail.com>
2024-03-06 17:48:33 +0000

    Modelos y relaciones realizadas

--- Backend/app/Models/Municipalitie.php
+++ Backend/app/Models/Municipalitie.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Municipalitie extends Model
 {
     use HasFactory;
+
+    protected $table = 'municipalities';
+
+    protected $fillable = [
+        "name",
+    ];
+
+    public function users(){
+
+        return $this->hasMany(User::class, 'municipality_id');
+
+    }
+
 }
--- Backend/app/Models/Post.php
+++ Backend/app/Models/Post.php
@@ -26,4 +26,11 @@ class Post extends Model
         return $this->belongsTo(Post_type::class, 'post_type_id');
 
     }
+
+    public function hashtags(){
+
+        return $this->belongsToMany(Hashtag::class, 'posts_hashtags', 'hashtag_id', 'post_id');
+
+    }
+
 }
