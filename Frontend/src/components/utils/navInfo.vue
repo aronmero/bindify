0975b7431c157a77c3cf72d3cd6f14c597f8396a
aronmero <aronmedinasp@gmail.com>
2024-03-07 11:21:32 +0000

    Parcial validarNotificacion Falta añadir el fetch para obtener los datos y el fetch para enviar si se valida o no. Ademas tambien se deberia eliminar del array que se muestra.

--- Frontend/src/components/utils/navInfo.vue
+++ Frontend/src/components/utils/navInfo.vue
@@ -4,8 +4,10 @@ const array = ["Notificaciones", "Solicitudes verificación"];
 </script>
 
 <template>
-    <div><img src="@public/assets/icons/arrowLeft.svg" class="lg:hidden absolute left-[20px]">
-        <div class="ml-[10px] gap-[25px] flex justify-center"><div v-for=" item in array" class="text-[12px] font-semibold">{{ item }}</div></div>
+    <div><a @click="$router.back()"><img src="@public/assets/icons/arrowLeft.svg" class="lg:hidden absolute left-[20px]  cursor-pointer"></a>
+        <div class="ml-[10px] gap-[25px] flex justify-center">
+            <div v-for=" item in array" class="text-[12px] font-semibold cursor-pointer">{{ item }}</div>
+        </div>
     </div>
 </template>
 
--- Frontend/src/views/validate.vue
+++ Frontend/src/views/validate.vue
@@ -1,17 +1,48 @@
 <script setup>
 import Grid from "@/components/comun/layout.vue";
-
 import Header from "@/components/comun/header.vue";
 import Footer from "@/components/comun/footer.vue";
 import navInfo from "@/components/utils/navInfo.vue";
 import Notificacion from "@/components/utils/validarNotificacion.vue";
+import { ref, onMounted } from "vue";
+
+const notificaciones = ref([]);
+onMounted(async () => {
+    notificaciones.value = await funcionApi();
+})
+
+//Falta añadir el fetch a la api y eliminar del array el elemento
+const tryValidateComercio = async (parametro) => {
+    try {
+        //const response = await fetch("api/v1/usuarios");
+        //const data = await response.json();
+        console.log(parametro)
+    } catch (error) {
+
+    }
+};
+
+
+/**
+ * Funcion placeholder hasta tener una api
+ */
+function funcionApi(){
+    const array = [];
+    for (let index = 0; index < 40; index++) {
+         array.push(index);
+    }
+    return array
+};
 </script>
 
 <template>
     <Header />
     <Grid><template v-slot:Left></template>
         <navInfo></navInfo>
-        <Notificacion></Notificacion>
+        <div>
+            <Notificacion v-for="notificacion in notificaciones" @submit="tryValidateComercio" />
+        </div>
+
 
         <template v-slot:Right></template>
     </Grid>
