373648712b77856fbc223630782cb2b690e93c8f
Ronald <ronaldgutierrezmartin02@gmail.com>
2024-03-12 13:47:30 +0000

    vista perfil comercion funcionando

--- Frontend/src/components/perfiles/containers/contenedorVistaPosts.vue
+++ Frontend/src/components/perfiles/containers/contenedorVistaPosts.vue
@@ -1,16 +1,22 @@
 <script setup>
-  import FeedPost from "@/components/perfiles/containers/TipoPost.vue"
-  import { posts } from '@/components/perfiles/helpers/posts.js';
-  import { getUserPosts } from "@/Api/perfiles/perfil.js";
-  import { ref } from "vue";
-  let userPosts= ref(null);
-  async function responseCatcher() {
-  userPosts.value = await getUserPosts("get");
+import FeedPost from "@/components/perfiles/containers/TipoPost.vue";
+import { posts } from "@/components/perfiles/helpers/posts.js";
+import { getUserPosts } from "@/Api/perfiles/perfil.js";
+import router from "@/router/index.js";
+import { ref } from "vue";
+let userPosts = ref(null);
+let linkUsername = ref(router.currentRoute.value.params.username);
+console.log(linkUsername.value);
+async function responseCatcher() {
+  userPosts.value = await getUserPosts(
+    "get",
+    `/api/user/${linkUsername.value}/posts`
+  );
   console.log(userPosts.value);
 }
 responseCatcher();
 </script>
 <template>
-  <FeedPost v-for="post in userPosts" :post="post"></FeedPost>
+  <FeedPost v-if="userPosts!=null" v-for="post in userPosts" :post="post"></FeedPost>
 </template>
 <style scoped></style>
