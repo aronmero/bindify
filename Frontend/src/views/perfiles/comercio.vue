9d64e16218f0e6782e41093c16ec8ef028d79d06
Ronald <ronaldgutierrezmartin02@gmail.com>
2024-03-12 22:08:04 +0000

    antes de unificar perfiles

--- Frontend/src/views/perfiles/comercio.vue
+++ Frontend/src/views/perfiles/comercio.vue
@@ -44,7 +44,7 @@ function pintar(evento) {
 
 async function responseCatcher(metodo, subRuta) {
   userData.value = await getUserData(metodo, subRuta);
-  //console.log(userData.value);
+  console.log(userData.value);
 }
 
 if (linkUsername.value == userLogeado.usuario.username) {
@@ -55,40 +55,40 @@ if (linkUsername.value == userLogeado.usuario.username) {
   userExterno.value = true;
 }
 
-const isEventos = ref(false)
-const isPosts = ref(false)
-const isResenias = ref(false)
+const isEventos = ref(false);
+const isPosts = ref(false);
+const isResenias = ref(false);
 
 /**
  * Oculta todos los componentes
  */
 function ocultar() {
-  isEventos.value = false
-  isPosts.value = false
-  isResenias.value = false
+  isEventos.value = false;
+  isPosts.value = false;
+  isResenias.value = false;
 }
 
 /**
  * Ejecuta una serie de funciones que requieren de un evento.
  * Cambia un estilo, oculta todos los contenedores, y muestra uno en concreto
- * @param {*} evento 
+ * @param {*} evento
  */
 function manipulacion(evento) {
-  pintar(evento)
+  pintar(evento);
 
   ocultar();
   switch (evento.target.value) {
     case "1":
-    console.log(evento.target.value)
-    isEventos.value = true
+      console.log(evento.target.value);
+      isEventos.value = true;
       break;
     case "2":
-    console.log(evento.target.value)
-    isPosts.value = true
+      console.log(evento.target.value);
+      isPosts.value = true;
       break;
     case "3":
-    console.log(evento.target.value)
-    isResenias.value = true
+      console.log(evento.target.value);
+      isResenias.value = true;
       break;
 
     default:
@@ -100,41 +100,78 @@ function manipulacion(evento) {
 
 <template>
   <Header />
-  <Grid><template v-slot:Left></template>
+  <Grid
+    ><template v-slot:Left></template>
     <btnAtras titulo="Perfil"></btnAtras>
+
     <div class="flex flex-col gap-6" v-if="userData != null">
       <div>
-        <imgsPerfil :rutaBaner="userData[0].banner" altTextBaner="foto baner" :rutaPerfil="userData[0].avatar"
-          altTextPerfil="foto perfil"></imgsPerfil>
+        <imgsPerfil
+          :rutaBaner="userData[0].banner"
+          altTextBaner="foto baner"
+          :rutaPerfil="userData[0].avatar"
+          altTextPerfil="foto perfil"
+        ></imgsPerfil>
       </div>
 
       <div class="flex flex-col gap-10 justify-evenly">
         <div class="flex flex-col">
-          <textoEnNegrita :texto="userData[0].username" class="text-base lg:text-xl" />
+          <textoEnNegrita
+            :texto="userData[0].username"
+            class="text-base lg:text-xl"
+          />
         </div>
-        <div class="flex flex-col justify-center lg:items-start gap-10 lg:gap-20 lg:flex-row">
+        <div
+          class="flex flex-col justify-center lg:items-start gap-10 lg:gap-20 lg:flex-row"
+        >
           <div class="flex justify-center lg:items-start gap-28 lg:gap-20">
             <div class="flex flex-col">
-              <textoNormal :texto="userData[0].addess" class="text-sm lg:text-base" />
-              <textoNormal :texto="userData[0].phone" class="text-sm lg:text-base" />
-              <textoNormal :texto="userData[0].email" class="text-sm lg:text-base" />
+              <textoNormal
+                :texto="userData[0].address"
+                class="text-sm lg:text-base"
+              />
+              <textoNormal
+                :texto="userData[0].phone"
+                class="text-sm lg:text-base"
+              />
+              <textoNormal
+                :texto="userData[0].email"
+                class="text-sm lg:text-base"
+              />
             </div>
-            <contenedorPuntuacion :puntuacion="userData[0].avg" :cantidadResenias="userData[0].review_count" />
+            <contenedorPuntuacion
+              v-if="userData[0].tipo != 'ayuntamiento'"
+              :puntuacion="userData[0].avg"
+              :cantidadResenias="userData[0].review_count"
+            />
           </div>
           <div class="flex flex-row gap-4 justify-center lg:flex-col lg:gap-0">
             <textoEnNegrita texto="Horario:" class="text-sm lg:text-base" />
             <div class="flex flex-col">
-              <textoNormal texto="Lun-Vier: 8:00-15:00" class="text-sm lg:text-base" />
-              <textoNormal texto="Sabado: 8:00-12:00" class="text-sm lg:text-base" />
+              <textoNormal
+                texto="Lun-Vier: 8:00-15:00"
+                class="text-sm lg:text-base"
+              />
+              <textoNormal
+                texto="Sabado: 8:00-12:00"
+                class="text-sm lg:text-base"
+              />
             </div>
           </div>
         </div>
         <div class="flex justify-center items-center gap-40 lg:gap-20">
           <div class="flex flex-col">
-            <textoNormal :texto="userData[0].categories_name" class="text-sm lg:text-base" />
+            <textoNormal
+              :texto="userData[0].categories_name"
+              class="text-sm lg:text-base"
+            />
           </div>
           <div class="flex flex-col">
-            <textoNormal v-for="hashtag in userData[0].hashtags" :texto="hashtag" class="text-sm lg:text-base" />
+            <textoNormal
+              v-for="hashtag in userData[0].hashtags"
+              :texto="hashtag"
+              class="text-sm lg:text-base"
+            />
           </div>
         </div>
         <div class="flex gap-6 justify-center hidden">
@@ -146,24 +183,45 @@ function manipulacion(evento) {
         <!-- <contenedorBtnsPerfilUser></contenedorBtnsPerfilUser> -->
         <div class="flex justify-center">
           <RouterLink to="/perfil/edit" v-if="!userExterno">
-            <btnConText texto="EDIT PROFILE" class="transition hover:bg-accent-400 ease-linear hover:text-text-50 w-48">
+            <btnConText
+              texto="EDIT PROFILE"
+              class="transition hover:bg-accent-400 ease-linear hover:text-text-50 w-48"
+            >
             </btnConText>
           </RouterLink>
           <RouterLink to="" v-if="userExterno">
-            <btnConText texto="Segir" class="transition hover:bg-accent-400 ease-linear hover:text-text-50 w-48">
+            <btnConText
+              texto="Segir"
+              class="transition hover:bg-accent-400 ease-linear hover:text-text-50 w-48"
+            >
             </btnConText>
           </RouterLink>
         </div>
       </div>
 
       <div class="flex w-full justify-center gap-6">
-        <textoEnNegrita @click="manipulacion" texto="Posts" class="text-sm lg:text-base"
-          :class="`text-sm lg:text-base  ${estilos.hoverLinks}`" value="1" />
-        <textoEnNegrita @click="manipulacion" texto="Eventos" class="text-sm lg:text-base"
-          :class="`text-sm lg:text-base  ${estilos.hoverLinks}`" value="2" />
-        <textoEnNegrita @click="manipulacion" texto="Reseñas" class="text-sm lg:text-base"
-          :class="`text-sm lg:text-base  ${estilos.hoverLinks}`" value="3" />
-
+        <textoEnNegrita
+          @click="manipulacion"
+          texto="Posts"
+          class="text-sm lg:text-base"
+          :class="`text-sm lg:text-base  ${estilos.hoverLinks}`"
+          value="1"
+        />
+        <textoEnNegrita
+          @click="manipulacion"
+          texto="Eventos"
+          class="text-sm lg:text-base"
+          :class="`text-sm lg:text-base  ${estilos.hoverLinks}`"
+          value="2"
+        />
+        <textoEnNegrita
+          v-if="userData[0].tipo != 'ayuntamiento'"
+          @click="manipulacion"
+          texto="Reseñas"
+          class="text-sm lg:text-base"
+          :class="`text-sm lg:text-base  ${estilos.hoverLinks}`"
+          value="3"
+        />
       </div>
       <!--<RouterView></RouterView>-->
       <posts v-if="isPosts"></posts>
