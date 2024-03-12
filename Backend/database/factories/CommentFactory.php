bfa73af5cd243e04bf67d1f333ed599e2d2bf1df
Christian <chrisrengifo188@gmail.com>
2024-03-12 05:45:16 +0000

    Modify-> Varios Factorys modificados

--- Backend/database/factories/CommentFactory.php
+++ Backend/database/factories/CommentFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -19,12 +20,28 @@ class CommentFactory extends Factory
     public function definition(): array
     {
 
+        $auxReply = rand(1, 10);
+        $father_id = null;
+
+        if (Comment::count()>1) {
+            if($auxReply < 4){
+                $father_id = $this->faker->numberBetween(1, Comment::count());
+            }
+        }
+
+        $auxActive = rand(1, 12);
+        $active = false;
+
+        if($auxActive > 1){
+            $active = true;
+        }
+
         return [
             'user_id' => $this->faker->numberBetween(1, User::count()),
             'post_id' => $this->faker->numberBetween(1, Post::count()),
             'content' => $this->faker->text(),
-            'comment_id' => null,
-            'active' => $this->faker->boolean(),
+            'father_id' => $father_id,
+            'active' => $active,
         ];
     }
 }
