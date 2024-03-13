62a5e597ad4581d338de52578cc2ae0910ddbab7
amanso23 <adrianmanso80@gmail.com>
2024-03-13 19:07:15 +0000

    post integrados

--- Frontend/src/components/search/postCard.vue
+++ Frontend/src/components/search/postCard.vue
@@ -6,22 +6,27 @@
     import StarSVG from '@public/assets/icons/star.svg';
     import StarEmptySVG from '@public/assets/icons/starEmpty.svg';
 
-    import MoreSVG from '@public/assets/icons/ellipsis.svg';
 
 
 const redirect = (url) => {
     router.push(url)
 }
 
+
 import router from '@/router';
 
 import TipoOferta from '@public/assets/icons/tipo_oferta.svg'
 import TipoEvento from '@public/assets/icons/tipo_evento.svg'
+import { setRandomGradient } from '@/utils/randomGradient.js';
+
+
+
 
 </script>
 
 
 <template>
+   
     <div class=" post-header w-[100%] h-[60px] flex items-center">
         <div class=" avatar-wrapper w-[50px] h-[50px] rounded-full overflow-hidden mr-2 ">
             <img @click="redirect(`perfil/${post.username}/comercio`)" class=" cursor-pointer w-[100%] h-[100%] object-cover  "
@@ -31,36 +36,34 @@ import TipoEvento from '@public/assets/icons/tipo_evento.svg'
             <b @click="redirect(`perfil/${post.username}/comercio`)" class="cursor-pointer">{{ post.username }}</b>
             <small>{{ datetranslatesql(post.start_date) }}</small>
             <button click="" class=" rating flex h-[fit-content] items-center justify-start " v-if="post.avg != null">
-                <img class="  " :src="StarSVG" v-for="index in Math.floor(post.avg)" alt="star" loading="lazy"
+                <img class="" :src="StarSVG" v-for="index in Math.floor(post.avg)" alt="star" loading="lazy"
                     :title="post.avg" />
                 <img class=" " :src="StarEmptySVG" v-for="index in (5 - Math.floor(post.avg))" alt="star" loading="lazy"
                     :title="post.avg" />
                 <small>({{ post.avg.toFixed(2) }})</small>
             </button>
         </div>
-        <button @click="() => abrirModal(post.id)" class="mr-4">
-            <img :src="MoreSVG" alt="dots">
-        </button>
     </div>
     <div
-        class=" post-content w-[100%] h-[300px] sm:h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[600px] rounded-2xl  overflow-hidden mt-5 ">
+        class=" post-content w-[100%] h-[300px] sm:h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[600px] rounded-2xl  overflow-hidden  ">
         <img @click="redirect(`post/${post.post_id}`)" class=" cursor-pointer w-[100%] h-[100%] object-cover "
             :src="post.image" :alt="post.titulo">
     </div>
     <!-- Contenido del Post -->
     <div class="information">
-        <h3>
+        <h3 class="flex items-center gap-x-2 font-bold">
             <img :src="post.post_type === 'Post' ? TipoOferta : TipoEvento" alt="icono-evento" loading="lazy"
-                class="size-10">
+                class="size-8 rounded-full bg-[#fe822f] p-1">
             {{ post.title }}
         </h3>
         <span>
             {{ post.description }}
         </span>
-        <div class=" hashtags flex mb-10">
-            <a class=" hashtag mr-[10px] font-bold " :href="`busqueda/${hashtag}`" v-for="hashtag in post.hashtags"> #{{
+        <div class=" hashtags flex mb-10 mt-2">
+            <a :style="{ background: setRandomGradient() }" class="hashtag mr-[10px] font-bold text-white rounded-md text-xs p-1 hover:text-white"  v-for="hashtag in post.hashtags"> #{{
                 hashtag }}</a>
         </div>
 
     </div>
+    
 </template>
